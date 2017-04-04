cbabd7710639639a3a3a19614907757b050bb715
Matthew Boles <mboles@brightcove.com>
2017-04-04 16:29:21 -0400

    Update for new doc structure

--- generate-docs.sh
+++ generate-docs.sh
@@ -8,8 +8,6 @@ grunt
 echo ${txtyel} Finished grunt process
 echo ${txtred} Copying docs to Developer Docs directories....
 echo ${txtyel} Copying api docs to Video Cloud docs....
-cp -R docs/api/ ../BCL-developer-docs/en/video-cloud/brightcove-player/reference/api/
-echo ${txtyel} Copying api docs to Perform docs....
-cp -R docs/api/ ../BCL-developer-docs/en/perform/brightcove-player/reference/api/
+cp -R docs/api/ ../BCL-developer-docs/en/player/brightcove-player/reference/api/
 echo ${txtgrn} Finished!
 echo ${txtrst}
